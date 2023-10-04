230ca5adb763293202cdc74ce1113449059b7264
Vu Binh <22520129@gm.uit.edu.vn>
2023-10-04 13:38:27 +0700

    Thêm class Diem

--- Lab1/Bai1/Program.cs
+++ Lab1/Bai1/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Bai1
+﻿using System;
+namespace Bai01
 {
-    internal class Program
+    public class Diem
     {
-        static void Main(string[] args)
+        private float x;
+        private float y;
+        public void Nhap()
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Nhap toa do x: ");
+            x = float.Parse(Console.ReadLine());
+            Console.Write("Nhap toa do y: ");
+            y = float.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Toa do x: {0}, y: {1}", x, y);
+        }
+        public double KhoangCach(Diem A)
+        {
+            return Math.Sqrt(Math.Pow(A.x - x, 2) + Math.Pow(A.y - y, 2));
         }
     }
+
+
 }
