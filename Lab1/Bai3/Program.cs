c90f2ce35ca13433b0ee896fd79994ed45361a88
Dung Tien The <22520291@gm.uit.edu.vn>
2023-10-05 23:15:51 +0700

    Lab01/Bai03: Viết hàm tìm max(làm ý 1)

--- Lab1/Bai3/Program.cs
+++ Lab1/Bai3/Program.cs
@@ -5,6 +5,22 @@ using System.Linq;
 class Program
 {
 
+    //hàm tìm max trong dãy
+    public static T findMax<T>(List<T> l) where T : IComparable<T>
+    {
+        T max = l[0];
+
+        for (int i = 1; i < l.Count; i++)
+        {
+            if (max.CompareTo(l[i]) < 0)//nếu max<l[i]
+            {
+                max = l[i];
+            }
+        }
+
+        return max;
+    }
+
 
     //hàm tìm min trong dãy
     public static T findMin<T>(List<T> l) where T : IComparable<T>
@@ -27,6 +43,10 @@ class Program
         List<int> dsSoNguyen = new List<int> { 5, 2, 8, 1000, 3 };
         List<double> dsSoThuc = new List<double> { 3.5, 1.2, -4.8, 0.9 };
         List<string> dsChuoi = new List<string> { "a", "bb", "ccc", "dddd" };
+
+        Console.WriteLine("So nguyen lon nhat trong day la: " + findMax(dsSoNguyen));
+        Console.WriteLine("So thuc lon nhat trong day la: " + findMax(dsSoThuc));
+        Console.WriteLine("Chuoi lon nhat trong day la: " + findMax(dsChuoi));
 
         Console.WriteLine("So nguyen nho nhat trong day la: " + findMin(dsSoNguyen));
         Console.WriteLine("So thuc nho nhat trong day la: " + findMin(dsSoThuc));
