621a5ce255a894bc1b19cd108d77330d8728a46c
tqthy <22521461@gm.uit.edu.vn>
2023-10-15 05:10:21 -0700

    Upload full bai 5

--- Lab1/Bai5/Program.cs
+++ Lab1/Bai5/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Bai5
+﻿
+namespace Bai5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("~~~ Chuong trinh hien thi cac tap tin trong thu muc ~~~");
+            Console.WriteLine("Hay nhap duong dan thu muc: ");
+            string? path = Console.ReadLine();
+            try
+            {
+                var items = Directory.EnumerateFileSystemEntries(path);
+                
+                foreach (string currentFile in items)
+                {
+                    Console.Write(currentFile.Substring(path.Length+1) + " ");
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
