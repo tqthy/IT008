689a807d872c24a3a70196b43bc37d5ad17421ed
Dung Tien The <22520291@gm.uit.edu.vn>
2023-11-06 08:36:53 +0700

    Lab2/Bai2.9:Xây dựng các phương thức mã hóa và giải mã AES-128 trong file AES128.CS

---
+++ Lab2/Bai2.9/AES128.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2._9
+{
+    public class AES128
+    {
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("1234567890123456");
+        private static readonly byte[] IV = Encoding.UTF8.GetBytes("1234567890123456");
+
+        public static string EncryptString(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+
+                        return Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+            }
+        }
+
+        public static string DecryptString(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void EncryptToFile(string plainText, string filePath)
+        {
+            string encryptedText = EncryptString(plainText);
+            using (FileStream fStream = new FileStream("data.txt", FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fStream))
+                {
+                    sw.WriteLine(encryptedText);
+                }
+            }
+        }
+
+        public static string[] DecryptFromFile(string filePath)
+        {
+            var decryptedStrings = new List<string>();
+
+            using (FileStream fStream = new FileStream("data.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fStream))
+                {
+                    string line = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        decryptedStrings.Add(DecryptString(line));
+                    }
+                }
+            }
+            return decryptedStrings.ToArray();
+        }
+
+    }
+
+}
+
+
